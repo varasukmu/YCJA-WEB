b570bc8bef28e410bc1d94a89e9ef3b60dde3c36
Varaporn Sukmuang <wrp.sukmuang@gmail.com>
2025-02-13 00:54:39 +0700

    final vercel

--- app/admin/edit/[id]/page.tsx
+++ app/admin/edit/[id]/page.tsx
@@ -1,15 +1,37 @@
+// "use client";
+
+// import { NextPage } from "next";
+
+// interface EditPostProps {
+//   params: { id: string };
+// }
+
+// const EditPost: NextPage<EditPostProps> = ({ params }) => {
+//   const { id } = params;
+
+//   return <div>Post ID: {id}</div>;
+// };
+
+// export default EditPost;
+
+
 "use client";
 
-import { NextPage } from "next";
+import { useRouter } from "next/router";
 
-interface EditPostProps {
-  params: { id: string };
-}
+const EditPost = () => {
+  const router = useRouter();
+  const { id } = router.query;
 
-const EditPost: NextPage<EditPostProps> = ({ params }) => {
-  const { id } = params;
-
-  return <div>Post ID: {id}</div>;
+  return (
+    <div>
+      {id ? (
+        <p>Post ID: {id}</p>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
 
 export default EditPost;
